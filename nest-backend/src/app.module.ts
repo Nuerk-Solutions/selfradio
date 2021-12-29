eb82334aad0ba059f55dc500c44a9d75ed94e732
Thomas Nürk <jojopaul88@gmail.com>
2021-12-29 17:34:47 +0100

    Added .env file and integration

--- nest-backend/src/app.module.ts
+++ nest-backend/src/app.module.ts
@@ -4,11 +4,19 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MusicModule } from './music/music.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost/selfradio'),
+    MongooseModule.forRoot(`${process.env.MONGO_URL}`, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }),
     MusicModule,
+    // Two files are defined below, for the configuration of the app in different environments
+    ConfigModule.forRoot({
+      envFilePath: ['../.env', '.env'],
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
--- nest-backend/src/main.ts
+++ nest-backend/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000); // Use port from environment variable or 3000 to guarantee that the app is always running
 }
 
 bootstrap();
