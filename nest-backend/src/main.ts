a592308dba4c538775aa9635d9fc151b0c446d43
Thomas Nürk <jojopaul88@gmail.com>
2021-12-29 18:26:46 +0100

    Added console log to identify server state

--- nest-backend/src/main.ts
+++ nest-backend/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  console.log('Server Ready!'); // Log this message in order to display to correct server state
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   await app.listen(process.env.PORT || 3000); // Use port from environment variable or 3000 to guarantee that the app is always running
