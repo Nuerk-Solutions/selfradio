c4822e04f52de56ce9022e96b5e50da715553c64
Nick <nick.ruider@gmail.com>
2022-01-03 13:15:36 +0100

    Added some Components + ListenComponent + EditComponent + NavbarComponent + Routing

--- angular-frontend/src/app/app-routing.module.ts
+++ angular-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListenComponent } from "./listen/listen.component";
+import { EditComponent } from "./edit/edit.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'listen',
+    pathMatch: 'full'
+  },
+  {
+    path: 'listen',
+    component: ListenComponent,
+  },
+  {
+    path: 'edit',
+    component: EditComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- angular-frontend/src/app/app.module.ts
+++ angular-frontend/src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from "@angular/common/http";
+import { ListenComponent } from './listen/listen.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { EditComponent } from './edit/edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListenComponent,
+    NavbarComponent,
+    EditComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
